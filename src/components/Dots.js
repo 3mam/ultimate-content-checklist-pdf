e9b760fb6374fa82966ccf4216456118cb26bbb5
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2020-11-19 18:57:57 +0100

    add Dots component

---
+++ src/components/Dots.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Dot = styled.div`
+	border-radius: 50%;
+	width: 15px;
+  height: 15px;
+	margin-bottom: 22px;
+`;
+
+const Green = styled(Dot)`
+  background-color: #25eb98;
+	`;
+
+const White = styled(Dot)`
+  background-color: #eff0f1;
+	`;
+
+const Div = styled.div`
+	display: flex;
+	flex-direction: column;
+`
+
+const selectGreenColorForDots = (dotNumber) => {
+	const dost = new Array(3).fill({});
+	return dost.map((v, i) => dotNumber === i ? (<Green key={i} />) : (<White key={i} />));
+}
+
+const Dots = ({ selectDot = 0 }) => {
+	return (
+		<Div>
+			{selectGreenColorForDots(selectDot)}
+		</Div>
+	);
+}
+
+export default Dots;
