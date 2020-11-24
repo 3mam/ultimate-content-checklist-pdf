b179b7d4c55d6bd3134604304a889263d29e9fc3
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2020-11-24 21:18:16 +0100

    added animation for label in email

--- src/components/section1/Email.js
+++ src/components/section1/Email.js
@@ -4,7 +4,9 @@ import { graphql, useStaticQuery } from 'gatsby';
 import device from '../../utils/device';
 
 const Div = styled.div`
-		display: flex;
+	display: flex;
+	position: relative;
+
 	@media ${device.mobile} {
 		flex-direction: column;
 		align-items: stretch;
@@ -23,16 +25,40 @@ const Button = styled.button`
 	}
 `;
 
+const Span = styled.span`
+  position: absolute;
+  opacity: 0.6;
+  //top: 0;
+  left: 20px;
+	bottom: 23px;
+	@media ${device.mobile} {
+		bottom: 100px;
+	}
+  cursor: text;
+  transition: all 0.5s ease-in-out ;
+`;
+
 const Input = styled.input`
   width: 310px;
   height: 60px;
   border-radius: 10px;
   background-color: #ffffff;
 	margin: 0 14px 0 0;
+	padding: 0 0 0 20px;
+	font-size: 1.5em;
 	@media ${device.mobile} {
 		margin: 0 0 19px 0;
 		width: auto;
 	}
+
+	&:not(:placeholder-shown) + ${Span.toString()},
+	&:focus + ${Span.toString()} {
+		opacity: 1;
+		transform: translate(-10px, -45px);
+		font-size: 0.8em;
+		color: white;
+		cursor: default;
+	}	
 `;
 
 const Email = () => {
@@ -49,7 +75,8 @@ const Email = () => {
 
 	return (
 		<Div>
-			<Input placeholder={data.cms.emailPlaceholder} />
+			<Input type="text" placeholder=" " />
+			<Span>{data.cms.emailPlaceholder}</Span>
 			<Button>{data.cms.emailButtonText}</Button >
 		</Div>
 	);
