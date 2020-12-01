4012899f9f9ed111f5abbc8937c37e2d67b35202
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2020-12-01 19:15:34 +0100

    added suport for set email in url

--- src/functions/email.js
+++ src/functions/email.js
@@ -1,52 +1,66 @@
 function sendEmail(email) {
-  const https = require('https');
-  const data = JSON.stringify({
-    email: email,
-  });
+	const https = require('https');
+	const data = JSON.stringify({
+		email: email,
+	});
 
-  const options = {
-    hostname: 'api.mailerlite.com',
-    port: 443,
-    path: '/api/v2/groups/105134414/subscribers',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-MailerLite-ApiKey': process.env.API_MAILERLITE,
-    },
-  };
+	const options = {
+		hostname: 'api.mailerlite.com',
+		port: 443,
+		path: '/api/v2/groups/105134414/subscribers',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'X-MailerLite-ApiKey': process.env.API_MAILERLITE,
+		},
+	};
 
-  return new Promise((resolve, reject) => {
-    const req = https.request(options, (res) => {
-      let data = null;
-      res.on('data', (d) => {
-        data += d;
-      });
+	return new Promise((resolve, reject) => {
+		const req = https.request(options, (res) => {
+			let data = null;
+			res.on('data', (d) => {
+				data += d;
+			});
 
-      res.on('error', (e) => {
-        reject(e);
-      });
+			res.on('error', (e) => {
+				reject(e);
+			});
 
-      res.on('end', () => {
-        resolve({ code: res.statusCode, data: data });
-      });
-    });
-    req.write(data);
-    req.end();
-  });
+			res.on('end', () => {
+				resolve({ code: res.statusCode, data: data });
+			});
+		});
+		req.write(data);
+		req.end();
+	});
+}
+
+function returnDefaultRespond() {
+	return {
+		statusCode: 400,
+		body: 'No email!',
+	};
+}
+
+function getEmailFromEvent({ multiValueHeaders, multiValueQueryStringParameters }) {
+	if (multiValueHeaders.email !== undefined)
+		return multiValueHeaders.email[0];
+	else if (multiValueQueryStringParameters.email !== undefined)
+		return multiValueQueryStringParameters.email[0];
+	else
+		return false;
 }
 
 exports.handler = async function (event) {
-  if (event.multiValueHeaders.email === undefined)
-    return {
-      statusCode: 400,
-      body: 'No email!',
-    };
+	console.log(event);
+	const emailFromEvent = getEmailFromEvent(event);
+	if (!emailFromEvent)
+		return returnDefaultRespond();
 
-  const [email] = event.multiValueHeaders.email;
-  const { code, data } = await sendEmail(email);
+	const { code, data } = await sendEmail(emailFromEvent);
 
-  return {
-    statusCode: code,
-    body: data,
-  };
+	return {
+		statusCode: code,
+		body: data,
+	};
 };
